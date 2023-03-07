e0069430c5e065f37b1109d90cf132bfaa7c361e
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-03-07 06:59:03 +0000

    INITIAL

---
+++ root/mods-unpacked/Darkly77-FontReplacementDemo/mod_main.gd
@@ -0,0 +1,38 @@
+extends Node
+
+const MOD_DIR = "Darkly77-FontReplacementDemo/" # name of the folder that this file is in
+const D77_FONTDEMO_MODID = "Darkly77-FontReplacementDemo" # full ID of your mod (AuthorName-ModName)
+
+
+func _init(_modLoader = ModLoader):
+	ModLoaderUtils.log_info("Init", D77_FONTDEMO_MODID)
+	replace_fonts_preload()
+
+
+func _ready()->void:
+	ModLoaderUtils.log_info("Ready", D77_FONTDEMO_MODID)
+
+
+# Replace fonts with our custom one
+func replace_fonts_preload() -> void:
+	var font_resources = [
+		preload("res://resources/fonts/actual/base/font_biggest.tres"),
+		preload("res://resources/fonts/actual/base/font_big_outlined.tres"),
+		preload("res://resources/fonts/actual/base/font_big_title.tres"),
+		preload("res://resources/fonts/actual/base/font_button.tres"),
+		preload("res://resources/fonts/actual/base/font_floating_text.tres"),
+		preload("res://resources/fonts/actual/base/font_menus.tres"),
+		preload("res://resources/fonts/actual/base/font_smallest_text.tres"),
+		preload("res://resources/fonts/actual/base/font_small_button.tres"),
+		preload("res://resources/fonts/actual/base/font_small_text.tres"),
+		preload("res://resources/fonts/actual/base/font_small_title.tres"),
+		preload("res://resources/fonts/actual/base/font_subtitle.tres"),
+	]
+
+	var replacement_font = "res://mods-unpacked/Darkly77-TranslationsDemo/fonts/Caveat-Medium.ttf"
+
+	# Loop over vanilla font files
+	for font_res in font_resources:
+		# Replace the font source
+		font_res.font_data = load(replacement_font)
+		ModLoaderUtils.log_info("Replaced font: " + font_res.resource_path, D77_FONTDEMO_MODID)
